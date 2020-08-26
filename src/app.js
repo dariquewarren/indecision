40792153ca25a77306f8afa08f7f6e84e2627aca
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-08-25 21:32:49 -0400

    tested out usage of import and export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('i am iron man')
+// import square from './utils.js'
+import {isAdult, canDrink} from './person.js'
+// import {square, add} from './utils.js'
+console.log(isAdult(18))
+console.log(canDrink(18))
+// console.log('i am iron man')
+// console.log(square(5))
+// console.log(add(5,5))
+
+// create person.js
+// named exports--isadult. true if adult, false if not. canDrink if 21 or older true, els false
+// import isadult and can drink here(app.js)
+//print the result of bothe to console
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+console.log('utils page')
+
+export const square = (x)=>{
+    return x* x
+}
+export const add=(a,b)=>{
+    return a+b
+}
+
+// export {square, add}
+
+
